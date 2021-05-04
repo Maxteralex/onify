c3d3074977e7d447b4fa68caab93fb21e4b4b8d3
Alexandre Heine <xandeaph@gmail.com>
2021-05-04 16:50:51 -0300

    marker saving and temp marker

--- lib/map.page.dart
+++ lib/map.page.dart
@@ -8,13 +8,67 @@ class MapPage extends StatefulWidget {
 
 class _MapPageState extends State<MapPage> {
   GoogleMapController mapController;
-  Set<Marker> markers = new Set<Marker>();
+  Set<Marker> saved_markers = new Set<Marker>();
+  Set<Marker> temp_marker = new Set<Marker>();
+  // atualmente aponta para Niteroi
   double lat = -22.899153475969282;
   double long = -43.107521571547025;
-  int id = 0;
+  int marker_id = 0;
 
   void _onMapCreated(GoogleMapController controller) {
     mapController = controller;
+  }
+
+  Widget mapWidget() {
+    return GoogleMap(
+      onMapCreated: _onMapCreated,
+      onCameraMove: (data) {
+        //print(data);
+      },
+      onTap: (position) {
+        final Marker marker = Marker(
+          markerId: new MarkerId(marker_id.toString()),
+          position: position,
+          infoWindow: InfoWindow(
+            title: "Teste",
+            snippet: "Niterói/RJ",
+          ),
+        );
+        setState(() {
+          temp_marker.clear();
+          temp_marker.add(marker);
+        });
+        marker_id++;
+        print(position);
+      },
+      initialCameraPosition: CameraPosition(
+        target: LatLng(lat, long),
+        zoom: 11.0,
+      ),
+      markers: saved_markers.union(temp_marker),
+    );
+  }
+
+  Widget mapButton(Function function, Icon icon, Color color) {
+    return ElevatedButton(
+      onPressed: function,
+      child: icon,
+      style: ButtonStyle(
+        shape: MaterialStateProperty.all(new CircleBorder()),
+        elevation: MaterialStateProperty.all(2.0),
+        backgroundColor: MaterialStateProperty.all(color),
+        padding: MaterialStateProperty.all(const EdgeInsets.all(7.0)),
+      )
+    );
+  }
+
+  void saveMarker() {
+    saved_markers.add(temp_marker.first);
+    temp_marker.clear();
+  }
+
+  void removeSavedMarker() {
+
   }
 
   @override
@@ -24,33 +78,27 @@ class _MapPageState extends State<MapPage> {
         title: Text("Google Maps"),
       ),
       body: Container(
-        child: GoogleMap(
-          onMapCreated: _onMapCreated,
-          onCameraMove: (data) {
-            print(data);
-          },
-          onTap: (position) {
-            final Marker marker = Marker(
-              markerId: new MarkerId(id.toString()),
-              position: position,
-              infoWindow: InfoWindow(
-                title: "Teste",
-                snippet: "Niterói/RJ",
-              ),
-            );
-            setState(() {
-              markers.add(marker);
-            });
-            id++;
-            print(position);
-          },
-          initialCameraPosition: CameraPosition(
-            target: LatLng(lat, long),
-            zoom: 11.0,
-          ),
-          markers: markers,
+        child: Stack(
+          children: [
+            SizedBox(
+              width: MediaQuery.of(context).size.width,
+              height: MediaQuery.of(context).size.height - 80,
+              child: mapWidget()
+            ),
+            Align(
+              alignment: Alignment.topRight,
+              child: Container(
+                  margin: EdgeInsets.fromLTRB(0.0, 20.0, 0.0, 0.0),
+                  child: Column(
+                    children: [
+                      mapButton(saveMarker, Icon(Icons.add_location), Colors.green),
+                      mapButton(removeSavedMarker, Icon(Icons.wrong_location), Colors.red),
+                    ],
+                  )),
+            )
+          ],
         ),
-    )
+      ),
     );
   }
 }
