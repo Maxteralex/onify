2f81f49895e30c34f77730a4f790c261b812bedc
Alexandre Heine <xandeaph@gmail.com>
2021-05-05 16:56:18 -0300

    navigation bar global e início da route page

--- lib/main.dart
+++ lib/main.dart
@@ -1,64 +1,80 @@
 import 'package:flutter/material.dart';
-import 'package:flutter/rendering.dart';
 import 'package:flutter_t1/map.page.dart';
+import 'package:flutter_t1/route.page.dart';
 
+Widget navigationBar(BuildContext context) {
+  double bottomBarHeight = 75; // set bottom bar height
+  bool _show = true;
+  
+  return Container(
+    height: bottomBarHeight,
+    width: MediaQuery.of(context).size.width,
+    child: _show
+        ?BottomNavigationBar(
+      currentIndex: 2,
+      type: BottomNavigationBarType.fixed,
+      items: [
+        BottomNavigationBarItem(
+          icon: new Icon(Icons.directions_bus_outlined),
+          label: 'Bus',
+        ),
+        BottomNavigationBarItem(
+          icon: new Icon(Icons.person),
+          label: 'Driver',
+        ),
+        BottomNavigationBarItem(
+          icon: new Icon(Icons.map),
+          label: 'Map',
+        ),
+        BottomNavigationBarItem(
+            icon: Icon(Icons.add_road), label: 'Route'),
+      ],
+    )
+        : Container(
+      color: Colors.white,
+      width: MediaQuery.of(context).size.width,
+    ),
+  );
+}
 
 void main() {
   runApp(MaterialApp(
     initialRoute: '/',
     routes: {
       '/': (context) => Home(),
+      // '/routes/': (context) => RoutePage(),
     },
   ));
 }
 
 class Home extends StatefulWidget {
-  // This widget is the root of your application.
   @override
   _HomeState createState() => _HomeState();
 }
 
 class _HomeState extends State<Home> {
-  bool _show = true;
-  double bottomBarHeight = 75; // set bottom bar height
-
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      bottomNavigationBar: Container(
-        height: bottomBarHeight,
-        width: MediaQuery.of(context).size.width,
-        child: _show
-            ?BottomNavigationBar(
-          currentIndex: 2, // this will be set when a new tab is tapped
-          type: BottomNavigationBarType.fixed,
-          items: [
-            BottomNavigationBarItem(
-              icon: new Icon(Icons.directions_bus_outlined),
-              label: 'Bus',
-            ),
-            BottomNavigationBarItem(
-              icon: new Icon(Icons.person),
-              label: 'Driver',
-            ),
-            BottomNavigationBarItem(
-              icon: new Icon(Icons.map),
-              label: 'Map',
-            ),
-            BottomNavigationBarItem(
-                icon: Icon(Icons.add_road), label: 'Route'),
-          ],
-        )
-            : Container(
-          color: Colors.white,
-          width: MediaQuery.of(context).size.width,
-        ),
-      ),
-
+      bottomNavigationBar: navigationBar(context),
       body: MapPage(),
+    );
+  }
+}
 
-      // This trailing comma makes auto-formatting nicer for build methods.
+class Route extends StatefulWidget {
+  @override
+  _RouteState createState() => _RouteState();
+}
+
+class _RouteState extends State<Route> {
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      bottomNavigationBar: navigationBar(context),
+      body: RoutePage(),
     );
   }
 }
---
+++ lib/route.page.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+
+class RoutePage extends StatefulWidget {
+  @override
+  _RoutePageState createState() => _RoutePageState();
+}
+
+class _RoutePageState extends State<RoutePage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold();
+  }
+}
