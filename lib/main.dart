729216c831deec8bc3c2eb6947641b32fa14d4d3
GustavoOliveira133 <42876849+GustavoOliveira133@users.noreply.github.com>
2021-05-05 01:03:53 -0300

    BottomNav working

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/rendering.dart';
 import 'package:flutter_t1/map.page.dart';
 
 
@@ -18,12 +19,47 @@ class Home extends StatefulWidget {
 }
 
 class _HomeState extends State<Home> {
+  bool _show = true;
+  double bottomBarHeight = 75; // set bottom bar height
+
+
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(primarySwatch: Colors.blue),
-      home: MapPage(),
+    return Scaffold(
+      bottomNavigationBar: Container(
+        height: bottomBarHeight,
+        width: MediaQuery.of(context).size.width,
+        child: _show
+            ?BottomNavigationBar(
+          currentIndex: 2, // this will be set when a new tab is tapped
+          type: BottomNavigationBarType.fixed,
+          items: [
+            BottomNavigationBarItem(
+              icon: new Icon(Icons.directions_bus_outlined),
+              label: 'Bus',
+            ),
+            BottomNavigationBarItem(
+              icon: new Icon(Icons.person),
+              label: 'Driver',
+            ),
+            BottomNavigationBarItem(
+              icon: new Icon(Icons.map),
+              label: 'Map',
+            ),
+            BottomNavigationBarItem(
+                icon: Icon(Icons.add_road), label: 'Route'),
+          ],
+        )
+            : Container(
+          color: Colors.white,
+          width: MediaQuery.of(context).size.width,
+        ),
+      ),
+
+      body: MapPage(),
+
+      // This trailing comma makes auto-formatting nicer for build methods.
     );
   }
 }
+
